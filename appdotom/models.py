32b6a2c669eab76a8ee333d7ce43cd6f9a687212
tomazmayr <129135069+tomazmayr@users.noreply.github.com>
2023-05-12 17:17:38 -0300

    Update models.py

--- appdotom/models.py
+++ appdotom/models.py
@@ -4,13 +4,14 @@ class Jogadores(models.Model):
   descricao = models.CharField(max_length = 50)
   nome = models.CharField(max_length = 20)
   time = models.CharField(max_length = 20)
-  
+  #cria a classe jogadores com as epsecificações passadas pelo usuário. Nesse caso, so foi utilizada a models.charfield com um limite máximo de caracteres
 class Times(models.Model):
   nome = models.CharField(max_length = 50)
   description = models.TextField()
   País = models.CharField(max_length = 50)
-
+# cria a classe times com as pesecificações. Nesse caso foram usadas a models.charfield e a models.textfield, que da a opcao do usuario escrever um paragrafo sobre a classe
 class Estadio(models.Model):
   nome = models.CharField(max_length = 50)
   local = models.TextField()
   capacidade = models.IntegerField()
+  #cria a class estadio. Neste caso foi utilizado tambem o models.integerfield para o usuario selecionar um numero inteiro e dizer a capacidade do estadio
